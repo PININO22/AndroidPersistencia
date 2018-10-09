0dc31b6b1033627e0aa6bfc9fc619e5050035b87
monica <baigorriamonica@gmail.com>
2018-10-09 17:03:29 -0300

    --

--- app/src/main/java/com/example/propietario/persistenciadb/application/BaseAplication.java
+++ app/src/main/java/com/example/propietario/persistenciadb/application/BaseAplication.java
@@ -2,5 +2,8 @@ package com.example.propietario.persistenciadb.application;
 
 import android.app.Application;
 
+import javax.crypto.AEADBadTagException;
+
 public class BaseAplication extends Application {
+
 }
