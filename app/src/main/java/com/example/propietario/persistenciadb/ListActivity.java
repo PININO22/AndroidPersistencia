67a7fa2224f28aff039f0378a3a9dfe5faee2463
Juan Tricarico <Tricampeon98@gmail.com>
2018-10-09 17:43:34 -0300

    Vistas v2 ListActivity

---
+++ app/src/main/java/com/example/propietario/persistenciadb/ListActivity.java
@@ -0,0 +1,27 @@
+package com.example.propietario.persistenciadb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.example.propietario.persistenciadb.application.BaseAplication;
+
+public class ListActivity extends AppCompatActivity {
+
+    private TextView lista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista);
+        lista=(TextView)findViewById(R.id.tvlista);
+
+        for(String it:((BaseAplication)getApplication()).nombreClientes()){
+            lista.setText(lista.getText()+it);
+        }
+    }
+
+
+}
